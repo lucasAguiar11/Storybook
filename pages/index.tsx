4ef49199889e563dff700f1cc67c5b13f11fc48a
Lucas Aguiar <lucas.abreu@redeceler.com.br>
2022-10-15 17:47:28 -0300

    Primeira história

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import Button from "../src/components/Button/Button";
+
 export default function HomeScreen() {
   return (
     <div>
       <h1>Home Screen</h1>
+      <Button>Botão simples</Button>
     </div>
   );
 }
